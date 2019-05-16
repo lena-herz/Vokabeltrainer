1746c042c00b1e0beb1c9b42af20dd98fe1e7f04
Lena Herz <lenasophie.herz@gmx.de>
2019-05-16 08:24:59 +0200

    packages für BufferedReader etc importiert

--- src/vokabeltrainer/Vokabeltrainer.java
+++ src/vokabeltrainer/Vokabeltrainer.java
@@ -5,16 +5,26 @@
  */
 package vokabeltrainer;
 
+import java.io.File;
+import java.io.FileNotFoundException; 
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.*;
+
 public class Vokabeltrainer {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Lektionen in Excel eingeben, als .csv speichern, einlesen mit bufferedReader, schreiben mit bufferedWriter
-        Kurs englisch = new Kurs("Englisch");
-        Lektion ersteLektion = new Lektion("ersteLektion");
+        //Lektionen in Excel eingeben, als .csv speichern, einlesen mit bufferedReader(new FileReader("Datei")), schreiben mit bufferedWriter
+        Kurs englisch = new Kurs("Englisch"); //muss noch nutzerbestimmt gemacht werden bzw bei Eingabe der Vokabel
+        Lektion ersteLektion = new Lektion("ersteLektion"); //s.o. 
         englisch.lekHinzufuegen(ersteLektion);
+        
         printMenu();
         int menu = SystemInReader.readInt();
         while(menu!=2){
@@ -24,6 +34,7 @@ public class Vokabeltrainer {
             }
             switch(menu){
                 case 1:
+                    //überlegen, ob wir diese Abfragen mit in die vokHinzufuegen() packen und dann ohne Parameter; dann wäre hier die main Methode übersichtlicher
                     System.out.println("Bedeutung der Vokabel in der Ausgangssprache?");
                     String ausgspr = SystemInReader.readString();
                     System.out.println("Bedeutung der Vokabel in der Zielsprache?");
@@ -43,7 +54,7 @@ public class Vokabeltrainer {
     public static void printMenu(){
         System.out.println("Welche Aktion möchten Sie ausführen?");
         System.out.println("1: neue Karteikarte anlegen");
-        System.out.println("2: Programm beenden");
+        System.out.println("2: Programm beenden"); //wenn Änderung, dann auch in while-Schleife oben
     }
     
 }
